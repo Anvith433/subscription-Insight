daf95a3443bd30899ad8e3ded3dc6e999f5068de
Anvith433 <anvithshetty932@gmail.com>
2026-03-23 18:16:10 +0100

    comment written

--- src/main/java/com/yourorg/UserSnapShot/AppUsageDTO.java
+++ src/main/java/com/yourorg/UserSnapShot/AppUsageDTO.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter @Setter
 public class AppUsageDTO {
-    private String packageName;
+    private String packageName; // no matter what is the appName , it may vary on country and all but package name is constant 
     private String appName;
     private int usageMinutes;
 }
