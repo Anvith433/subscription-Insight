175e2385d7f7434505dd3216ce2961a6bf62257c
Anvith433 <anvithshetty932@gmail.com>
2026-04-14 16:36:22 +0100

    push codes which are required

--- src/main/java/com/yourorg/SubscriptionBackendApplication.java
+++ src/main/java/com/yourorg/SubscriptionBackendApplication.java
@@ -1,5 +1,4 @@
-package com.yourorg.subscription_backend;
-
+package com.yourorg;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,11 +8,9 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SubscriptionBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SubscriptionBackendApplication.class, args);
     }
-
     @Bean
     public CommandLineRunner runFlyway(DataSource dataSource) {
         return args -> {
